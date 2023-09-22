d19cbac37023c5cde84927cb18a731479fef1197
Tobias Gerschner <TobiG77@users.noreply.github.com>
2023-09-23 11:16:10 +1200

    Add rust (#2)

--- bootstrap-devenv.sh
+++ bootstrap-devenv.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+set -euo pipefail
+
 nix-env -iA nixpkgs.glibcLocales 
 
-grep -q "^#DEVENV_ELIXIR" ~/.bashrc_custom
+grep -qE '^\. \$HOME\/\.bashrc_devenv' ~/.bashrc_custom 2>/dev/null || \
+    echo '. $HOME/.bashrc_devenv' >> ~/.bashrc_custom 
 
-cat >> ~/.bashrc_devenv_elixir <<"EOF"
+test -e ~/.bashrc_devenv || {
+
+cat > ~/.bashrc_devenv <<"EOF"
 
 LANG=en_NZ.UTF-8
 LANGUAGE=en_NZ.UTF-8
 LC_ALL=en_NZ.UTF-8
 
 export LOCALE_ARCHIVE="$(nix-env --installed --no-name --out-path --query glibc-locales)/lib/locale/locale-archive"
-
-echo '. ~/.bashrc_devenv_elixir' > ~/.bashrc_custom
-
 EOF
+
+}
